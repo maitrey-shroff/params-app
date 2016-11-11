2566c701d9073de0caee157e152e5b4069d45f8b
maitrey-shroff <maitrey89@me.com>
2016-11-10 19:26:31 -0800

    completed in class exercise

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -2,10 +2,6 @@ class ParamsController < ApplicationController
 
   def query_params
     @message = params[:message]
-    if @message.initial == "a"
-      puts "Hey! Your name starts with the first letter of the alphabet"
-    end
-
   end
 
 
