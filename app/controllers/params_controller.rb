6def896a563150cb5248a756cf81eec2db020600
maitrey-shroff <maitrey89@me.com>
2016-11-13 09:35:32 -0800

    changed game to form version of theg ame

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,9 +1,55 @@
 class ParamsController < ApplicationController
 
-  def query_params
-    @message = params[:message]
+  # def name_thing
+  #   @message = params[:message]
+  #   if @message.first == "a"
+  #     @response = "Hey! Your name starts with the first letter of the alphabet!"
+  #   end
+  # end
+
+  # def number_game
+  #   winning_number = 23
+  #   chosen_number = params[:number].to_i
+
+  #   if chosen_number > winning_number
+  #     @message = "too high, guess lower"
+  #   elsif chosen_number < winning_number
+  #     @message = "too low, guess higher"
+  #   else
+  #     @message = "you guessed correctly!"
+  #   end
+
+  def game
+    winning_number = 23
+    chosen_number = params[:number].to_i
+
+    if chosen_number > winning_number
+      @message = "too high, guess lower"
+    elsif chosen_number < winning_number
+      @message = "too low, guess higher"
+    else
+      @message = "you guessed correctly!"
+    end
   end
 
+  def form_display
+
+  end
+
+  def form_result
+
+    winning_number = 23
+    chosen_number = params[:answer].to_i
+
+    if chosen_number > winning_number
+      @message = "too high, guess lower"
+    elsif chosen_number < winning_number
+      @message = "too low, guess higher"
+    else
+      @message = "you guessed correctly!"
+    end
+
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/params_test", to: "params#query_params"
-  
+
+  get "/params_game/:number", to: "params#game"
+
+  get "/form_display", to: "params#form_display"
+  post "/form_result", to: "params#form_result"
 end
